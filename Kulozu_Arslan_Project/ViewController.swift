a660c8befa0567520ad2983676b7837c641b04a2
bkkulozu <bkklz>
2021-12-30 22:22:11 +0300

    Initial Commit

---
+++ Kulozu_Arslan_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kulozu_Arslan_Project
+//
+//  Created by Beste Kulozu on 30.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
